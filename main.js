fef1f22d901eaf695471913eadc71a3b548091d6
paresiqbal <thecrowdel@gmail.com>
2023-09-30 20:58:19 +0700

    Add const

---
+++ main.js
@@ -0,0 +1,6 @@
+const books = [];
+const RENDER_EVENT = "render-book";
+const STORAGE_KEY = "Bookshelf";
+const form = document.getElementById("inputBook");
+const inputSearchBook = document.getElementById("searchBookTitle");
+const formSearchBook = document.getElementById("searchBook");
