df447411bc122896f8c00a220ac9eb31fa2d1944
paresiqbal <thecrowdel@gmail.com>
2023-10-03 20:49:20 +0700

    asdasd

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const STORAGE = "Bookshelf";
 const form = document.getElementById("addBook");
 const searchBookTitle = document.getElementById("searchTitle");
 const searchBook = document.getElementById("searchBook");
+
+inputSearchBook.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  searchBooks();
+});
+
+formSearchBook.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchBooks();
+});
