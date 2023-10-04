8a4846742b084f33a7f1153673310176574d79e9
paresiqbal <thecrowdel@gmail.com>
2023-10-04 12:49:33 +0700

    asdasd

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ const STORAGE = "Bookshelf";
 const form = document.getElementById("addBook");
 const searchBookTitle = document.getElementById("searchTitle");
 const searchBook = document.getElementById("searchBook");
+const bookId = () => +new Date();
 
-inputSearchBook.addEventListener("keyup", (e) => {
-  e.preventDefault();
+searchBookTitle.addEventListener("keyup", (event) => {
+  event.preventDefault();
   searchBooks();
 });
 
-formSearchBook.addEventListener("submit", (e) => {
-  e.preventDefault();
+searchBook.addEventListener("submit", (event) => {
+  event.preventDefault();
   searchBooks();
 });
+
+// check webstorage
+function webstorage() {
+  if (typeof Storage === "undefined") {
+    console.log("Sorry Web Storage Doesn't Exists");
+    return false;
+  }
+
+  return true;
+}
