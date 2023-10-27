37436c4c52dba3a2cd742e37dc9b00c83e886b14
paresiqbal <thecrowdel@gmail.com>
2023-10-27 23:10:07 +0700

    asdsad

---
+++ main.js
@@ -0,0 +1,6 @@
+const books = [];
+const RENDER_BOOK = "render-book";
+const STORAGE_BOOK = "Bookshelf";
+const form = document.getElementById("inputBook");
+const inputSearchBook = document.getElementById("searchBookTitle");
+const formSearchBook = document.getElementById("searchBook");
